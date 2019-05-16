f637f90161eeb4479c2f8edd7992a023d50f26b8
Guilherme Ribeiro Paitax <guilhermepaitax@engefoto.com.br>
2019-05-16 09:07:45 -0300

    Add project files.

---
+++ Sudoku/FormMain.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Sudoku
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.btnResolver = new System.Windows.Forms.Button();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chkProcesso = new System.Windows.Forms.CheckBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnImportar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnResolver
+            // 
+            this.btnResolver.BackColor = System.Drawing.Color.White;
+            this.btnResolver.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnResolver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResolver.Font = new System.Drawing.Font("Arial", 13F);
+            this.btnResolver.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnResolver.Location = new System.Drawing.Point(50, 412);
+            this.btnResolver.Name = "btnResolver";
+            this.btnResolver.Size = new System.Drawing.Size(130, 41);
+            this.btnResolver.TabIndex = 1;
+            this.btnResolver.Text = "Resolver";
+            this.btnResolver.UseVisualStyleBackColor = false;
+            this.btnResolver.Click += new System.EventHandler(this.btnResolver_Click);
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.AllowUserToAddRows = false;
+            this.DataGridView1.AllowUserToDeleteRows = false;
+            this.DataGridView1.AllowUserToOrderColumns = true;
+            this.DataGridView1.AllowUserToResizeColumns = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Aquamarine;
+            this.DataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.DataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.ColumnHeadersVisible = false;
+            this.DataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Aquamarine;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.DataGridView1.GridColor = System.Drawing.Color.Aquamarine;
+            this.DataGridView1.Location = new System.Drawing.Point(369, 63);
+            this.DataGridView1.MultiSelect = false;
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.RowHeadersVisible = false;
+            this.DataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.DataGridView1.RowTemplate.Height = 43;
+            this.DataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.DataGridView1.Size = new System.Drawing.Size(426, 390);
+            this.DataGridView1.TabIndex = 6;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.MaxInputLength = 1;
+            this.Column1.Name = "Column1";
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column1.Width = 47;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.MaxInputLength = 1;
+            this.Column2.Name = "Column2";
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column2.Width = 47;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.MaxInputLength = 1;
+            this.Column3.Name = "Column3";
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column3.Width = 47;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.MaxInputLength = 1;
+            this.Column4.Name = "Column4";
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column4.Width = 47;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.MaxInputLength = 1;
+            this.Column5.Name = "Column5";
+            this.Column5.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column5.Width = 47;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.MaxInputLength = 1;
+            this.Column6.Name = "Column6";
+            this.Column6.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column6.Width = 47;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.MaxInputLength = 1;
+            this.Column7.Name = "Column7";
+            this.Column7.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column7.Width = 47;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column8";
+            this.Column8.MaxInputLength = 1;
+            this.Column8.Name = "Column8";
+            this.Column8.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column8.Width = 47;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Column9";
+            this.Column9.MaxInputLength = 1;
+            this.Column9.Name = "Column9";
+            this.Column9.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column9.Width = 47;
+            // 
+            // chkProcesso
+            // 
+            this.chkProcesso.AutoSize = true;
+            this.chkProcesso.BackColor = System.Drawing.Color.Transparent;
+            this.chkProcesso.Font = new System.Drawing.Font("Arial", 13F);
+            this.chkProcesso.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.chkProcesso.Location = new System.Drawing.Point(50, 353);
+            this.chkProcesso.Name = "chkProcesso";
+            this.chkProcesso.Size = new System.Drawing.Size(170, 25);
+            this.chkProcesso.TabIndex = 7;
+            this.chkProcesso.Text = "Mostrar Processo";
+            this.chkProcesso.UseVisualStyleBackColor = false;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.White;
+            this.btnLimpar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Arial", 13F);
+            this.btnLimpar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnLimpar.Location = new System.Drawing.Point(195, 412);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(130, 41);
+            this.btnLimpar.TabIndex = 8;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnImportar
+            // 
+            this.btnImportar.BackColor = System.Drawing.Color.White;
+            this.btnImportar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnImportar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnImportar.Font = new System.Drawing.Font("Arial", 13F);
+            this.btnImportar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnImportar.Location = new System.Drawing.Point(50, 277);
+            this.btnImportar.Name = "btnImportar";
+            this.btnImportar.Size = new System.Drawing.Size(130, 41);
+            this.btnImportar.TabIndex = 9;
+            this.btnImportar.Text = "Importar TXT";
+            this.btnImportar.UseVisualStyleBackColor = false;
+            this.btnImportar.Click += new System.EventHandler(this.btnImportar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(32, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(293, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(842, 489);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnImportar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.chkProcesso);
+            this.Controls.Add(this.DataGridView1);
+            this.Controls.Add(this.btnResolver);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sudoku";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnResolver;
+        internal System.Windows.Forms.DataGridView DataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.CheckBox chkProcesso;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnImportar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Sudoku/FormMain.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace Sudoku
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private int[,] MatrizSudoku = new int[9, 9];
+
+        private void btnResolver_Click(object sender, EventArgs e)
+        {
+            //Limpa a matriz
+            MatrizSudoku = new int[9, 9];
+            //Valida as informações do grid
+            if (!ValidaGrid())
+            {
+                MessageBox.Show("Formatação do quebra cabeça inválida", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //Chama função para resolver o Sudoku
+            if (Resolver(0, 0)) MessageBox.Show("Sudoku resolvido com sucesso!", "Sucesso!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else MessageBox.Show("Impossivel resolver Sudoku!", "Alerta!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+        }
+
+        //Valida e monta Matriz com base nos valores do Grid
+        private bool ValidaGrid()
+        {
+            int num;
+            for (int i = 0; i < DataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < DataGridView1.ColumnCount; j++)
+                {
+
+                    if (DataGridView1.Rows[i].Cells[j].Value == null || DataGridView1.Rows[i].Cells[j].Value == "")
+                    {
+                        MatrizSudoku[i, j] = 0;
+                    }
+                    else if (Int32.TryParse(DataGridView1.Rows[i].Cells[j].Value.ToString(), out num))
+                    {
+                        if (!ValidaNum(i, j, num)) return false;
+                        else MatrizSudoku[i, j] = num;
+                    }
+                    else return false;
+
+                }
+            }
+            return true;
+        }
+
+        private bool Resolver(int l, int c)
+        {
+
+            int coluna = c, linha = l;
+
+            //Condição de parada
+            if (c == 9) return true;
+            //Pega próxima posição
+            if (linha < 8) linha++;
+            else
+            {
+                linha = 0;
+                coluna++;
+            }
+
+            //Verifica se a posição do quebra cabeça esta vazia
+            if (MatrizSudoku[l, c] != 0) return Resolver(linha, coluna);
+            else
+            {
+                //Loop nos possíveis elementos a serem preenchidos
+                for (int num = 1; num < 10; num++)
+                {
+                    //Chama função para validar se elemento é válido
+                    if (ValidaNum(l, c, num))
+                    {
+                        MatrizSudoku[l, c] = num;
+                        DataGridView1.Rows[l].Cells[c].Value = num;
+                        DataGridView1.Rows[l].Cells[c].Style.ForeColor = Color.LightSeaGreen;
+                        if (chkProcesso.Checked)
+                        {
+                            //Delay para mostrar processo de construção do quebra cabeça
+                            var t = Task.Run(async delegate
+                            {
+                                await Task.Delay(100);
+                            });
+                            t.Wait();
+                        }
+                        //Chama função com proxíma posição
+                        if (Resolver(linha, coluna)) return true;
+                        //Caso não seja possível preencher casa, zera o valor para tentar novamente
+                        MatrizSudoku[l, c] = 0;
+                        DataGridView1.Rows[l].Cells[c].Value = "";
+                    }
+                }
+            }
+            return false;
+        }
+
+        //Retorna se um determinado número é valido no quebra cabeça
+        private bool ValidaNum(int l, int c, int num)
+        {
+            //Verifica se ele repete nas linhas e colunas
+            for (int i = 0; i < 9; i++)
+            {
+                if (MatrizSudoku[l, i] == num)
+                    return false;
+                if (MatrizSudoku[i, c] == num)
+                    return false;
+            }
+
+            //Pega o quadrante do número
+            int linha = (l / 3) * 3;
+            int coluna = (c / 3) * 3;
+
+            //Verifica se ele repete no quadrante
+            for (int j = linha; j < linha + 3; j++)
+            {
+                for (int k = coluna; k < coluna + 3; k++)
+                {
+                    if (MatrizSudoku[j, k] == num) return false;
+                }
+            }
+
+            return true;
+        }
+
+        //Limpa todas as células do Grid
+        private void LimparGrid()
+        {
+            for (int i = 0; i < DataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < DataGridView1.ColumnCount; j++)
+                {
+                    DataGridView1.Rows[i].Cells[j].Value = null;
+                    DataGridView1.Rows[i].Cells[j].Style.ForeColor = Color.FromArgb(64, 64, 64);
+                }
+            }
+        }
+
+        //Abre arquivo txt
+        private void btnImportar_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Text Files (*txt)|*.txt";
+            openFileDialog1.Title = "Sudoku TXT";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                if (File.Exists(openFileDialog1.FileName))
+                {
+                    LimparGrid();
+                    LerArquivo(openFileDialog1.FileName);
+                }
+            }
+        }
+
+        //Faz a leitura do arquivo txt e monta o Grid com base nele
+        private void LerArquivo(string arquivo)
+        {
+            StreamReader leitor = new StreamReader(arquivo);
+            string linha = leitor.ReadLine();
+
+            while (linha != null)
+            {
+
+                for (int i = 0; i < 9; i++)
+                {
+                    int index = 0;
+                    for (int j = 0; j < 9; j++)
+                    {
+                        try
+                        {
+                            if (linha.Substring(index, 1) != "0")
+                            {
+                                DataGridView1.Rows[i].Cells[j].Value = linha.Substring(index, 1);
+                                index += 1;
+                            }
+                            else
+                            {
+                                DataGridView1.Rows[i].Cells[j].Value = null;
+                                index += 1;
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            LimparGrid();
+                            MessageBox.Show("Formatação de arquivo inválida", "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+
+                        }
+                    }
+                    linha = leitor.ReadLine();
+                }
+
+            }
+        }
+
+        //Chama função para limpar o Grid
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparGrid();
+        }
+
+        //Monta Grid que representa o quebra cabeça Sudoku
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            DataGridView1.Rows.Add(9);
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    DataGridView1.Rows[i].Cells[j].Style.BackColor = Color.LightCyan;
+                    DataGridView1.Rows[i].Cells[j + 6].Style.BackColor = Color.LightCyan;
+                    DataGridView1.Rows[i + 3].Cells[j + 3].Style.BackColor = Color.LightCyan;
+                    DataGridView1.Rows[i + 6].Cells[j].Style.BackColor = Color.LightCyan;
+                    DataGridView1.Rows[i + 6].Cells[j + 6].Style.BackColor = Color.LightCyan;
+                }
+            }
+        }
+    }
+}
